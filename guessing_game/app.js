00648e3a277d59736c9773304e1bd7bb7412cfef
Elena Quevedo <elenaquevedo@Elenas-MacBook-Pro.local>
2016-09-15 20:41:25 -0700

    js question 5 added

--- guessing_game/app.js
+++ guessing_game/app.js
@@ -11,8 +11,7 @@ if (questionOne === 'yes' || questionOne ==='y') {
   alert('Yes,I\'m from outerspace!');
 }
 
-var questionTwo = prompt('Do I have 10 toes?');
-questionTwo = questionTwo.toLowerCase();
+var questionTwo = prompt('Do I have 10 toes?').toLowerCase();
 
 if (questionTwo === 'yes' || questionTwo ==='y') {
   alert('You got it!');
@@ -21,8 +20,7 @@ if (questionTwo === 'yes' || questionTwo ==='y') {
   alert('Actually, I got 10 extra toes!');
 }
 
-var questionThree = prompt('Do I like swimming?');
-questionThree = questionThree.toLowerCase();
+var questionThree = prompt('Do I like swimming?'.toLowerCase();
 
 if (questionThree === 'yes' || questionThree ==='y') {
   alert('Nope, I will start melting if I touch water!');
@@ -31,12 +29,19 @@ if (questionThree === 'yes' || questionThree ==='y') {
   alert('That\'s right! I don\'t like water in my ears and nose!');
 }
 
-var questionFour = prompt('Do I like running?');
-questionFour = questionFour.toLowerCase();
+var questionFour = prompt('Do I like running?').toLowerCase();
 
 if (questionFour === 'yes' || questionFour ==='y') {
   alert('I have a jet pack so I can fly!');
 
 } else if (questionFour === 'no' || questionFour ==='n'){
   alert('Not true, I believe I can fly, I believe I can touch the sky!');
+}
+
+var questionFive = prompt ("Is the color of my spaceship silver?");
+if (questionFive === 'yes' || questionFive ==='y') {
+  alert('My spaceship is Hot Pink!');
+
+} else if (questionFive === 'no' || questionFive ==='n'){
+  alert('That\'s right. I don\'t have a spaceship the mothership left me!' );
 }
